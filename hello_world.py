982505b923972d221f14c5f4afc1d2bbd4b42940
Levi Huddleston <levilhuddleston@gmail.com>
2023-03-13 15:46:28 -0700

    Test list not key value pair

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
 from scripts.argparse import create_parser
 print("hello world")
-param_value = dbutils.widgets.get("base_parameters")
+param_value = dbutils.widgets.get("python_params")
 print(param_value)
